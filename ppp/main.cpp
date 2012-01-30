7aaf05e73fc33f2ae5841a87d667882874ae3e7d
Timothée Ravier <timothee.romain.ravier@gmail.com>
2012-01-30 11:08:50 +0100

    ppp: remove uneeded char in ppp output

--- ppp/main.cpp
+++ ppp/main.cpp
@@ -87,23 +87,23 @@ int main(int argc, char ** argv)
 	s[0].link_offset = 0;
 // 	piga_seq_init(s);
 
-	std::cout << "[+] PPP: Beggining PIGA policy parsing and loading..." << std::endl;
+	std::cout << "PPP: Beggining PIGA policy parsing and loading..." << std::endl;
 	res = yyparse();
 
 	if(res != 0) {
-		std::cerr << "[+] PPP: Parsing failed!" << std::endl;
+		std::cerr << "PPP: Parsing failed!" << std::endl;
 		return -1;
 	}
 
-	std::cout << "[+] PPP: Policy successfully parsed. Setting up in the kernel..." << std::endl;
+	std::cout << "PPP: Policy successfully parsed. Setting up in the kernel..." << std::endl;
 	res = PPP::set_sequence(curr_seq, curr_link, s, l);
 
 	if(res == 0) {
-		std::cout << "[+] PPP: PIGA policy loadded:" << std::endl;
-		std::cout << "[+] \t" << PPP::get_loaded() << " signatures have been loaded" << std::endl;
-		std::cout << "[+] \t" << PPP::get_ignored() << " signatures have been ignored" << std::endl;
+		std::cout << "PPP: PIGA policy loadded:" << std::endl;
+		std::cout << "\t" << PPP::get_loaded() << " signatures have been loaded" << std::endl;
+		std::cout << "\t" << PPP::get_ignored() << " signatures have been ignored" << std::endl;
 	} else {
-		std::cerr << "[+] PPP: Failed to set up policy!" << std::endl;
+		std::cerr << "PPP: Failed to set up policy!" << std::endl;
 		return -1;
 	}
 
@@ -111,25 +111,25 @@ int main(int argc, char ** argv)
 		std::map< std::string, unsigned int > map;
 		std::map< std::string, unsigned int > sid_map;
 
-		std::cout << "[+] Policy stats : '<name> (<sid>): <count>'" << std::endl;
+		std::cout << "Policy stats: '<name> (<sid>): <count>'" << std::endl;
 		std::map< std::string, unsigned int>::const_iterator itr;
 		map = PPP::get_stat_sid();
 		sid_map = PPP::get_sid_map();
-		std::cout << "[+] \tSecurity context stats :" << std::endl;
+		std::cout << "\tSecurity context stats:" << std::endl;
 		for (itr = map.begin(); itr != map.end(); itr++) {
-			std::cout << "[+] \t\t"<< (*itr).first << " (" << sid_map[(*itr).first] << "): " << (*itr).second << std::endl;
+			std::cout << "\t\t"<< (*itr).first << " (" << sid_map[(*itr).first] << "): " << (*itr).second << std::endl;
 		}
 
 		map = PPP::get_stat_tclass();
-		std::cout << "[+] \ttclass stats :" << std::endl;
+		std::cout << "\ttclass stats:" << std::endl;
 		for (itr = map.begin(); itr != map.end(); itr++) {
-			std::cout << "[+] \t\t"<< (*itr).first << " (" << tclass_map[(*itr).first] << "): " << (*itr).second << std::endl;
+			std::cout << "\t\t"<< (*itr).first << " (" << tclass_map[(*itr).first] << "): " << (*itr).second << std::endl;
 		}
 
 		map = PPP::get_stat_requested();
-		std::cout << "[+] \trequested stats :" << std::endl;
+		std::cout << "\trequested stats:" << std::endl;
 		for (itr = map.begin(); itr != map.end(); itr++) {
-			std::cout << "[+] \t\t"<< (*itr).first << ": " << (*itr).second << std::endl;
+			std::cout << "\t\t"<< (*itr).first << ": " << (*itr).second << std::endl;
 		}
 	}
 
